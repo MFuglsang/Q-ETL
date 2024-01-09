c96d9e7b35e3e31427fa738bcd988fe158c43d7f
MFuglsang <morten.w.fuglsang@gmail.com>
2024-01-09 08:34:47 +0100

    Database configurations

--- python/config/configuration.py
+++ python/config/configuration.py
@@ -2,7 +2,23 @@
 def loadConfig():
     
     settings = {
-        "Qgs_PrefixPath" : "C:/App/OSGeo4w/apps/qgis"
+        "Qgs_PrefixPath" : "C:/App/OSGeo4w/apps/qgis",
+        "DatabaseConnections": {
+            "MyPostGIS" : {
+                "host" : "",
+                "port" : "",
+                "user" : "",
+                "password": ""
+            },
+            "MyMSSQL" : {
+                "host" : "",
+                "port" : "",
+                "databasename":"",
+                "user" : "",
+                "password": ""
+            }
+
+        }
     }
 
     return settings
