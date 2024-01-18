2b5f369151fd45ab739022af26e866337f8981c9
MFuglsang <morten.w.fuglsang@gmail.com>
2024-01-18 10:58:20 +0100

    Update boilerplate.py

--- python/boilerplate.py
+++ python/boilerplate.py
@@ -1,19 +1,53 @@
+#####################################
+## BOILERPLATE PART
+## DO NOT CHANGE 
+#####################################
+
+import sys, os
 from qgis.core import QgsApplication
 
-QgsApplication.setPrefixPath("<PATH TO QGIS FOLDER>", True)
+sys.path.append("python/config")
+from configuration import *
+
+sys.path.append("python/log")
+from filelog import *
+
+settings = loadConfig()
+settings['logfile'] = createLogFile(os.path.basename(__file__), settings['logdir'])
+infoWriter('Kicking off... ', 'INFO', settings)
+
+QgsApplication.setPrefixPath(settings["Qgs_PrefixPath"], True)
 qgs = QgsApplication([], False)
 qgs.initQgis()
+
+## Loading the Processing plugin...
+sys.path.append(settings["QGIS_Plugin_Path"])
+import processing
+from processing.core.Processing import Processing
+from processing.script.ScriptUtils import *
+Processing.initialize()
 
 ## Loading stuff on the running QGIS...
 sys.path.append("python/transformers")
 sys.path.append("python/readers")
 sys.path.append("python/writers")
-from geometry import reproject
-from inputreaders import readShapefile
-from outputwriters import writeShapefile
+import config
+from geometry import *
+from inputreaders import *
+from outputwriters import *
 
-## Code goes here...
+infoWriter("QGIS ready from CMD", 'INFO', settings)
 
+#####################################
+## SCRIPT PART (WRITE CODE HERE) 
+#####################################
+
+
+
+
+#####################################
+## EXITING THE SCRIPT
+#####################################
+
+endScript(settings)
 qgs.exitQgis()
-
-
