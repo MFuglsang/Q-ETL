d6b6917ea22bf49b39c7bf4f1dce6d45158aad75
MFuglsang <morten.w.fuglsang@gmail.com>
2024-01-20 13:39:55 +0100

    Update _local_configuration.py

--- python/config/_local_configuration.py
+++ python/config/_local_configuration.py
@@ -3,7 +3,7 @@ def loadConfig():
     settings = {
         "Qgs_PrefixPath" : "C:/App/OSGeo4w/apps/qgis",
         "QGIS_Plugin_Path" : 'C:/App/OSGeo4W/apps/qgis/python/plugins',
-        "logdir" : "C:/Users/Administrator/Documents/GitHub/QGIS__ETL/logs/",
+        "logdir" : "C:/Projects/QGIS_ETL_Python/logs/",
         "DatabaseConnections": {
             "MyPostGIS" : {
                 "host" : "",
