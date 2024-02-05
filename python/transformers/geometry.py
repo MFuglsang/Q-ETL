caf26554f305986e39a8c2deea11ede864b0afc3
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-05 21:22:37 +0100

    Update geometry.py

--- python/transformers/geometry.py
+++ python/transformers/geometry.py
@@ -40,12 +40,14 @@ def reproject(layer, targetEPSG, settings):
         sys.exit()
 
 def reprojectV2(layer, targetEPSG, settings):
+    infoWriter("Running reporjector V2", 'Info', settings)
     try:
         parameter = {
             'INPUT': layer,
             'TARGET_CRS': targetEPSG,
             'OUTPUT': 'memory:Reprojected'
         }
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
         result = processing.run('native:reprojectlayer', parameter)['OUTPUT']
         infoWriter("Reprojector V2 finished", 'Info', settings)
         return result
@@ -93,6 +95,7 @@ def isGeometryValid(layer):
     return summary
 
 def forceRHR(layer, settings):
+    infoWriter("Running force right-hand rule", 'Info', settings)
     try:
         parameter = {
             'INPUT': layer,
@@ -107,6 +110,7 @@ def forceRHR(layer, settings):
         sys.exit()
 
 def dissolveFeatures(layer, fieldList, settings):
+    infoWriter("Dissolving features", 'Info', settings)
     try:
         parameter = {
             'INPUT': layer,
@@ -114,6 +118,7 @@ def dissolveFeatures(layer, fieldList, settings):
             'SEPARATE_DISJOINT' : False,
             'OUTPUT': 'memory:dissolved'
         }
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
         result = processing.run('native:dissolve', parameter)['OUTPUT']
         infoWriter("dissolveFeatures finished", 'Info', settings)
         return result
@@ -124,7 +129,6 @@ def dissolveFeatures(layer, fieldList, settings):
 
 def bufferLayer(layer, distance, segements, endcapStyle, joinStyle, miterLimit, dissolve, settings):
     infoWriter("Creating buffer layer", 'Info', settings)
-    infoWriter("Parameters : " + str(distance) + 'm, ' + str(segements) + ' segments, ' + 'dissolve ' + str(dissolve)  , 'Info', settings)
     try:
         parameter = {
             'INPUT': layer,
@@ -136,6 +140,7 @@ def bufferLayer(layer, distance, segements, endcapStyle, joinStyle, miterLimit, 
             'DISSOLVE': dissolve,
                 'OUTPUT': 'memory:dissolved'
         }
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
         result = processing.run('native:buffer', parameter)['OUTPUT']
         infoWriter("BufferLayer finished", 'Info', settings)
         return result
