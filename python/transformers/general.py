414094dbe989b42983661806c43f7f3f70cf73dc
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-09 11:52:38 +0100

    Update general.py

--- python/transformers/general.py
+++ python/transformers/general.py
@@ -41,12 +41,14 @@ def scriptRunner(scritpName, params, settings):
         sys.exit()
 
 def extractByExpression(layer, expression, settings):
+    infoWriter("Extracting by expression", 'Info', settings)
     try:
         parameter = {
             'INPUT': layer,
             'EXPRESSION': expression,
             'OUTPUT': 'memory:extracted'
         }
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
         result = processing.run('native:extractbyexpression', parameter)['OUTPUT']
         infoWriter("Extractbyexpression  finished", 'Info', settings)
         return result
@@ -54,4 +56,66 @@ def extractByExpression(layer, expression, settings):
         infoWriter("An error occured in extractByExpression", 'ERROR', settings)
         infoWriter("Program terminated" , 'ERROR', settings)
         sys.exit()
+
+def addAutoIncrementalField(layer, fieldname, start, settings):
+    infoWriter("Adding incremental field", 'Info', settings)
+    try:
+        parameter = {
+            'INPUT': layer,
+            'FIELD_NAME': fieldname,
+            'START':start,
+            'MODULUS':0,
+            'GROUP_FIELDS':[],
+            'SORT_EXPRESSION':'',
+            'SORT_ASCENDING':True,
+            'SORT_NULLS_FIRST':False,
+            'OUTPUT': 'memory:extracted'
+        }
+        result = processing.run('native:addautoincrementalfield', parameter)['OUTPUT']
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
+        infoWriter("addAutoIncrementalField  finished", 'Info', settings)
+        return result
+    except:
+        infoWriter("An error occured in addAutoIncrementalField", 'ERROR', settings)
+        infoWriter("Program terminated" , 'ERROR', settings)
+        sys.exit()
+
+def deleteColumns (layer, columns, settings):
+    infoWriter("deleting fields", 'Info', settings)
+    try:
+        parameter = {
+            'INPUT': layer,
+            'COLUMN':columns,
+            'OUTPUT': 'memory:extracted'
+        }
+        result = processing.run('native:deletecolumn', parameter)['OUTPUT']
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
+        infoWriter("deleteColumns  finished", 'Info', settings)
+        return result
+    except:
+        infoWriter("An error occured in deleteColumns", 'ERROR', settings)
+        infoWriter("Program terminated" , 'ERROR', settings)
+        sys.exit()
+
+def fieldCalculator (layer, fieldname, fieldtype, fieldlength, fieldprecision, formula, settings):
+    infoWriter("Calculating field", 'Info', settings)
+    try:
+        parameter = {
+            'INPUT': layer,
+            'FIELD_NAME': fieldname,
+            'FIELD_TYPE': fieldtype,
+            'FIELD_LENGTH': fieldlength,
+            'FIELD_PRECISION': fieldprecision,
+            'FORMULA': formula,
+            'OUTPUT': 'memory:extracted'
+        }
+        result = processing.run('native:fieldcalculator', parameter)['OUTPUT']
+        infoWriter("Parameters: " + str(parameter), 'Info', settings)
+        infoWriter("fieldCalculator  finished", 'Info', settings)
+        return result
+    except:
+        infoWriter("An error occured in fieldCalculator", 'ERROR', settings)
+        infoWriter("Program terminated" , 'ERROR', settings)
+        sys.exit()
+    
 
