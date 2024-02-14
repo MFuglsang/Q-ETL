2cdd12c6fdda598ea54167e430efee87ec073688
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-14 08:05:56 +0100

    Remove workers based on feter-by-feature coding

--- python/workers/geometry.py
+++ python/workers/geometry.py
@@ -15,31 +15,8 @@ from filelog import *
 
 print("Geometry imported")
 
+
 def reproject(layer, targetEPSG, settings):
-    infoWriter("Running reprojector on layer, from " + str(layer.crs().authid()) + ' to ' +  str(targetEPSG), 'Info', settings)
-
-    try:
-        newLayer = layer.clone()
-        newLayer.startEditing()
-        source_crs = QgsCoordinateReferenceSystem(layer.crs().authid())
-        target_crs = QgsCoordinateReferenceSystem(targetEPSG)
-            
-        for feat in newLayer.getFeatures():
-            geom = feat.geometry() 
-            geom.transform(QgsCoordinateTransform(source_crs, target_crs, QgsProject.instance()))
-            feat.setGeometry(geom)
-            newLayer.updateFeature(feat)
-            
-        newLayer.setCrs(target_crs)
-        newLayer.commitChanges()
-        infoWriter("Reprojector finished", 'Info', settings)
-        return newLayer
-    except:
-        infoWriter("An error occured reprojectiong layer", 'ERROR', settings)
-        infoWriter("Program terminated" , 'ERROR', settings)
-        sys.exit()
-
-def reprojectV2(layer, targetEPSG, settings):
     infoWriter("Running reporjector V2", 'Info', settings)
     try:
         parameter = {
@@ -56,43 +33,6 @@ def reprojectV2(layer, targetEPSG, settings):
         infoWriter("Program terminated" , 'ERROR', settings)
         sys.exit()
 
-
-def createCentroid(layer, settings):
-    infoWriter("Creating centroids", 'Info', settings)
-    try:
-        newLayer = QgsVectorLayer(f'Point?crs={layer.crs().authid()}', 'newLayer', 'memory')
-        newLayer_data = newLayer.dataProvider()
-        newLayer_data.addAttributes(layer.fields())
-        newLayer.updateFields()
-
-        newLayer.startEditing()
-        for f in layer.getFeatures():
-            geom = f.geometry()
-            centroid_geom = geom.centroid()
-            centroid_feature = QgsFeature()
-            centroid_feature.setGeometry(centroid_geom)
-            centroid_feature.setAttributes(f.attributes())
-            newLayer_data.addFeature(centroid_feature)
-        
-        newLayer.commitChanges()
-        infoWriter("Centroids finished", 'Info', settings)
-        return newLayer
-    except:
-        infoWriter("An error occured creating centroids", 'ERROR', settings)
-        infoWriter("Program terminated" , 'ERROR', settings)
-        sys.exit()   
-
-def isGeometryValid(layer):
-    summary = []
-    for feat in layer.getFeatures():
-        geom = feat.geometry()
-        validator = QgsGeometryValidator(geometry=geom)
-        errors = [[error.what(), error.where()] for error in validator.validateGeometry(geometry=geom)]
-         
-        for e in errors:
-            summary.append(f"Feature id {feat.id()} Error {e}")
-
-    return summary
 
 def forceRHR(layer, settings):
     infoWriter("Running force right-hand rule", 'Info', settings)
