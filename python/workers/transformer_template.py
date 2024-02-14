4ced969f0a1fe97127e5d84a33edea6f87861cb8
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-14 10:28:46 +0100

    New analysis workers

--- python/workers/transformer_template.py
+++ python/workers/transformer_template.py
@@ -1,12 +1,12 @@
-def TRANSFORMERNAME(layer, settings): ## Layer and settings are mandatory
-    filelog.infoWriter("Running TRANSFORMERNAME", 'Info', settings)
+def WORKERNAME(layer, settings): ## Layer and settings are mandatory
+    filelog.infoWriter("Running WORKERNAME", 'Info', settings)
     try:
 
         ## Transformer-code go's here
 
-        filelog.infoWriter("TRANSFORMERNAME finished", 'Info', settings)
+        filelog.infoWriter("WORKERNAME finished", 'Info', settings)
         return result ## Transformer must return something 
     except:
-        filelog.infoWriter("An error occured in TRANSFORMERNAME", 'ERROR', settings)
+        filelog.infoWriter("An error occured in WORKERNAME", 'ERROR', settings)
         filelog.infoWriter("Program terminated" , 'ERROR', settings)
         sys.exit()
