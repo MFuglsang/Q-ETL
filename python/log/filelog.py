46418619f5e8a4894d2904af3b3583fb6d89c0ea
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-16 14:39:12 +0100

    Added system information to engine description

--- python/log/filelog.py
+++ python/log/filelog.py
@@ -1,5 +1,7 @@
 
 from datetime import datetime
+import platform,socket,re,uuid,json
+import pip._internal as pip
 
 def createLogFile(scriptname, logdir):
     now = datetime.now()
@@ -9,7 +11,7 @@ def createLogFile(scriptname, logdir):
     f.write("##################################################\n")
     f.write("QGIS ETL JOB LOG                               \n")
     f.write("JOB ID: " + scriptname +  "                    \n")
-    f.write("STARTTIME: " + now.strftime("%d%m%Y, %H:%M") +  " \n")
+    f.write("STARTTIME: " + now.strftime("%d/%m/%Y, %H:%M") +  " \n")
     f.write("##################################################\n")
     f.write("\n")
     f.close()
@@ -37,7 +39,7 @@ def endScript(settings):
     now = datetime.now()
     f = open(settings['logfile'], "a")
     f.write("\n")
-    f.write("ENDTIME: " + now.strftime("%d%m%Y, %H:%M") +  " \n")
+    f.write("ENDTIME: " + now.strftime("%d/%m/%Y, %H:%M") +  " \n")
     f.write("##################################################\n")
     f.write("Job completed with succes!                        \n")
     f.write("##################################################\n")
@@ -45,10 +47,39 @@ def endScript(settings):
     print("Job completed with succes!")
 
 def describeEngine(scriptfolder, algorithms, version, settings):
+
+    try:
+        import psutil
+    except ImportError:
+        pip.main(['install', 'psutil'])
+        import psutil
+
+    info={}
+    info['platform']=platform.system()
+    info['platform-release']=platform.release()
+    info['platform-version']=platform.version()
+    info['architecture']=platform.machine()
+    info['hostname']=socket.gethostname()
+    info['ip-address']=socket.gethostbyname(socket.gethostname())
+    info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+    info['processor']=platform.processor()
+    try:
+        info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+        info['cores'] = psutil.cpu_count()
+    except:
+        info['ram'] ='Not available'
+
     f = open(settings['logfile'], "a")
     f.write("\n")
     f.write("##################################################\n")
     f.write("Initializing engine:                              \n")
+    f.write("Platform: " + info['platform'] + " " + info['platform-release'] + " \n")
+    f.write("Platform version: " + info['platform-version'] + " \n")
+    f.write("Architecture: " + info['architecture'] + " \n")
+    f.write("Processor: " + info['processor'] +  " \n")
+    f.write("Number of cores : " + str(info['cores']) + " \n")
+    f.write("Available memmory: " + info['ram'] + " \n")
+    f.write("\n")
     f.write("QGIS version: " + str(version) + "                \n")
     f.write("Script folder: " + str(scriptfolder) + "\n")
     algs = []
