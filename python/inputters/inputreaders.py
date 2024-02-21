d5bceaa233465aad8f4c4fc1d4f000fbd7eb9414
Daníel Örn Árnason <danielarnason85@gmail.com>
2024-02-21 16:36:48 +0100

    module docstring and typo

--- python/inputters/inputreaders.py
+++ python/inputters/inputreaders.py
@@ -1,3 +1,6 @@
+"""
+This module contains readers for the supported formats.
+"""
 from random import randrange
 from owslib.wfs import WebFeatureService
 import sys, os
@@ -53,6 +56,7 @@ def wfs(uri, settings):
     ----------
     uri : str
         The uri can be a HTTP url to a WFS server (http://foobar/wfs?TYPENAME=xxx&SRSNAME=yyy[&FILTER=zzz) or a URI constructed using the QgsDataSourceURI class with the following parameters: - url=string (mandatory): HTTP url to a WFS server endpoint. e.g http://foobar/wfs - typename=string (mandatory): WFS typename - srsname=string (recommended): SRS like ‘EPSG:XXXX’ - username=string - password=string - authcfg=string - version=auto/1.0.0/1.1.0/2.0.0 -sql=string: full SELECT SQL statement with optional WHERE, ORDER BY and possibly with JOIN if supported on server - filter=string: QGIS expression or OGC/FES filter - restrictToRequestBBOX=1: to download only features in the view extent (or more generally in the bounding box of the feature iterator) - maxNumFeatures=number - IgnoreAxisOrientation=1: to ignore EPSG axis order for WFS 1.1 or 2.0 - InvertAxisOrientation=1: to invert axis order - hideDownloadProgressDialog=1: to hide the download progress dialog
+        
     settings
         The configuration object defined in your config file (config_boilerplate.py)
     
