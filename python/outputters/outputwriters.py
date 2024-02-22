64a1c387d3b2e975b1e54cb6b9851a4c97daecdc
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-22 19:43:18 +0100

    Update outputwriters with docstrings

--- python/outputters/outputwriters.py
+++ python/outputters/outputwriters.py
@@ -16,6 +16,23 @@ import sys
 from log import filelog
 
 def file(layer, path, format, settings):
+    """_summary_
+
+    Parameters
+    ----------
+    layer : QgsVectorLayer
+        The QgsVectorLayer that is to be written to a file
+
+    path : _type_
+        The full path for the file to be created
+
+    format : _type_
+        The driver type used to write the data to the file. 
+
+    settings
+        The configuration object defined in your config file (config_boilerplate.py)
+    """
+
     filelog.infoWriter("Writing " + str(layer.featureCount()) + " features to: " + path, 'Info', settings)
     try:
         QgsVectorFileWriter.writeAsVectorFormat(layer, path, "utf-8", layer.crs(), format)
@@ -26,22 +43,29 @@ def file(layer, path, format, settings):
         filelog.infoWriter("Program terminated" , 'ERROR', settings)
         sys.exit()
 
-def temp(layer, name,  settings):
-    filelog.infoWriter("Writing temp output to: " + settings['TempFolder'] + '/persistent/', 'Info', settings)
- 
-    if os.path.exists(settings['TempFolder'] + '/persistent/' + name + '.geojson'):
-        filelog.infoWriter("Temp file exists, deleting it", 'Info', settings)
-        os.remove(settings['TempFolder'] + '/persistent/' + name + '.geojson')
-    try:
-        QgsVectorFileWriter.writeAsVectorFormat(layer, settings['TempFolder'] + '/persistent/' + name + '.geojson' , "utf-8", layer.crs(), "GeoJson")
-        filelog.infoWriter("Export to temp completed", 'Info', settings)
-    except Exception as error:
-        filelog.infoWriter("An error occured exporting layer to temp", 'ERROR', settings)
-        filelog.infoWriter(type(error).__name__ + " – " + str(error) , 'ERROR', settings)
-        filelog.infoWriter("Program terminated" , 'ERROR', settings)
-        sys.exit()
-
 def geopackage(layer, layername, geopackage, overwrite, settings):
+    """
+     A function that writes a QgsVectorLayer to a Geopackage file. 
+
+    Parameters
+    ----------
+    layer : QgsVectorLayer
+        The QgsVectorLayer that is to be written to the geopackage
+
+    layername : String
+        The name of the layer in the geopackage file
+
+    geopackage : String
+        The full path for the geopackage to be created
+
+    overwrite : Boolean
+
+        Specify wheather the writer will overwrite existing geopackage or append layer. Boolean True/False
+
+    settings
+        The configuration object defined in your config file (config_boilerplate.py)
+    """
+
     filelog.infoWriter("Writing "+ str(layer.featureCount()) + " features to geopackage : " + geopackage  , 'Info', settings)
     try:
         layer.setName(layername)
