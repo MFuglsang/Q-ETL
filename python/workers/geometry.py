b0e787df8298d423ed049fbe01f0cd494d804e26
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-23 11:31:49 +0100

    Update geometry.py

--- python/workers/geometry.py
+++ python/workers/geometry.py
@@ -71,6 +71,47 @@ def dissolveFeatures(layer, fieldList, disjoined, settings):
         sys.exit()
 
 def bufferLayer(layer, distance, segements, endcapStyle, joinStyle, miterLimit, dissolve, settings):
+    """
+    Computes a buffer area for all the features in an input layer, using a fixed or data defined distance.
+    It is possible to use a negative distance for polygon input layers.
+    In this case the buffer will result in a smaller polygon (setback).
+    QGIS processing algorithem: native:buffer
+
+    Parameters
+    ----------
+    layer : Qgsvectorlayer [vector: any]
+        The Qgsvectorlayer input for the algorithem
+
+    distance : Integer
+        The buffer distance. Default: 10.0
+
+    segements : Integer
+        Number og segments. Default: 5
+
+    endcapStyle : Enumeration
+        Controls how line endings are handled in the buffer. Default: 0 
+        (One of: 0 — Round, 1 — Flat, 2 — Square)
+
+    joinStyle : Enumeration
+        Specifies whether round, miter or beveled joins should be used when offsetting corners in a line.
+        Default: 0 (Options are: 0 — Round, 1 — Miter, 2 — Bevel)
+
+    miterLimit : Integer
+        Sets the maximum distance from the offset geometry to use when creating a mitered join as a factor of the offset distance
+        Default: 0, Minimum: 1
+
+    dissolve : Boolean
+        Dissolve the final buffer. Default: false.
+
+    settings : dict
+        The configuration object defined in your config file (config_boilerplate.py)
+
+    Returns
+    -------
+    Qgsvectorlayer [vector: polygon]
+        The result output from the algorithem
+    """
+
     filelog.infoWriter("Creating buffer layer", 'Info', settings)
     filelog.infoWriter("Processing " + str(layer.featureCount()) +" features", 'Info', settings)
     try:
@@ -95,6 +136,25 @@ def bufferLayer(layer, distance, segements, endcapStyle, joinStyle, miterLimit, 
         sys.exit()
 
 def fixGeometry(layer, settings):
+    """
+    Attempts to create a valid representation of a given invalid geometry without losing any of the input vertices.
+    Already valid geometries are returned without further intervention. Always outputs multi-geometry layer.
+    QGIS processing algorithem: native:fixgeometries
+
+    Parameters
+    ----------
+    layer : Qgsvectorlayer [vector: any]
+        The Qgsvectorlayer input for the algorithem
+
+    settings : dict
+        The configuration object defined in your config file (config_boilerplate.py)
+
+    Returns
+    -------
+    Qgsvectorlayer [vector: any]
+        The result output from the algorithem
+
+    """
     filelog.infoWriter("Fixing geometries", 'Info', settings)
     filelog.infoWriter("Processing " + str(layer.featureCount()) +" features", 'Info', settings)
     try:
