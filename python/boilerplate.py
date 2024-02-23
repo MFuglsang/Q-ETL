8873dcb6df1775183a2838f5b775133f491a7947
Daníel Örn Árnason <danielarnason85@gmail.com>
2024-02-23 16:08:06 +0100

    nye imports

--- python/boilerplate.py
+++ python/boilerplate.py
@@ -1,55 +1,13 @@
-#####################################
-## BOILERPLATE PART
-## DO NOT CHANGE 
-#####################################
+from python.qgis_init import *
 
-import sys, os
-from qgis.core import QgsApplication, Qgis
-from config import _local_configuration
-from log import filelog
-from misc import misc
-
-settings = _local_configuration.loadConfig()
-settings['logfile'] = filelog.createLogFile(os.path.basename(__file__), settings['logdir'])
-misc.validateEnvironment(settings)
-
-QgsApplication.setPrefixPath(settings["Qgs_PrefixPath"], True)
-qgs = QgsApplication([], False)
-qgs.initQgis()
-
-## Loading the Processing plugin...
-sys.path.append(settings["QGIS_Plugin_Path"])
-import processing
-from processing.core.Processing import Processing
-from processing.script.ScriptUtils import *
-from qgis.analysis import QgsNativeAlgorithms
-
-Processing.initialize()
-QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
-from processing.script import ScriptUtils
-
-filelog.describeEngine(ScriptUtils.scriptsFolders(), QgsApplication.processingRegistry().providerById("script").algorithms(), Qgis.QGIS_VERSION,  settings)
-filelog.infoWriter('Loading python ressources', 'INFO', settings)
-
-from workers import analysis, attributes, general, geometry
 from inputters import inputreaders
 from outputters import outputwriters
-from constructors import construct
 
-misc.initCompleted(settings)
+## Reading from WFS into a QGIS layer
+wfslayer = inputreaders.wfs("srsname='EPSG:25832' typename='VD:vma_admdata' url='http://vmgeoserver.vd.dk/geoserver/VD/wfs' version='1.0.0' sql=SELECT * FROM vma_admdata WHERE BESTYRER=330", settings)
 
-#####################################
-## SCRIPT PART (WRITE CODE HERE) 
-#####################################
-
-
-
-
-#####################################
-## EXITING THE SCRIPT
-#####################################
+outputwriters.geopackage(wfslayer, 'vma_admdata', 'C:/Users/daniar/Desktop/temp/vejman_wfs.gpkg', True, settings)
 
 qgs.exitQgis()
-
 filelog.endScript(settings)
 misc.cleanUp(settings)
