3b702c2ffa6ba47466494c4e480958e6fecfa661
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-25 13:10:09 +0100

    Update boilerplate.py

--- python/boilerplate.py
+++ python/boilerplate.py
@@ -1,13 +1,4 @@
-from python.qgis_init import *
+from core import *
+from engine import *
 
-from inputters import inputreaders
-from outputters import outputwriters
-
-## Reading from WFS into a QGIS layer
-wfslayer = inputreaders.wfs("srsname='EPSG:25832' typename='VD:vma_admdata' url='http://vmgeoserver.vd.dk/geoserver/VD/wfs' version='1.0.0' sql=SELECT * FROM vma_admdata WHERE BESTYRER=330", settings)
-
-outputwriters.geopackage(wfslayer, 'vma_admdata', 'C:/Users/daniar/Desktop/temp/vejman_wfs.gpkg', True, settings)
-
-qgs.exitQgis()
-filelog.endScript(settings)
-misc.cleanUp(settings)
+## Code goes here...
