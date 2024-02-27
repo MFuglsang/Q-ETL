5c01951fb975c4088b94696e18b3e6b8fcf65058
MFuglsang <morten.w.fuglsang@gmail.com>
2024-02-27 12:41:24 +0100

    Load order of core jobs

--- python/core/__init__.py
+++ python/core/__init__.py
@@ -9,11 +9,12 @@ logger = initialize_logger(settings)
 start_logfile()
 
 from core.misc import validateEnvironment, describeEngine, get_postgres_connections, get_bin_folder
-validateEnvironment(settings)
 settings['bin_path'] = get_bin_folder(settings)
+validateEnvironment(settings)
+
 
 settings['Postgres_Ponnections'] = get_postgres_connections(settings)
-print(settings)
+
 
 QgsApplication.setPrefixPath(settings["Qgs_PrefixPath"], True)
 qgs = QgsApplication([], False)
